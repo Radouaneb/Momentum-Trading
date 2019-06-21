db0b766d1c9901e2b97c6c0f17d9017d640c2a52
Radouaneb <50450781+Radouaneb@users.noreply.github.com>
2019-06-21 16:26:09 +0100

    Add files via upload

---
+++ email_file.py
@@ -0,0 +1,42 @@
+import smtplib
+import os
+import pandas
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+
+SUBSCRIBE_LIST = [
+    'bensallami.radouane1@gmail.com'
+]
+
+def email_login():
+    me = 'test'
+    import getpass
+    password = ''
+    return me, password
+
+def send_mail(send_from, send_to, subject, text, files=[], server="localhost", port=587, username='', password='', isTls=True):
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(f, "rb").read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if isTls: smtp.starttls()
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+# find_validETF(path + 'ETF.csv')
